60055ab2096fb3e24c0c3edf2d5381bc8025bbd9
Elton Andrade <eltonsa75@hotmail.com>
2019-08-07 12:45:28 -0300

    Operadore Comparativos

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Estudo_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int a = 10;
+           bool c1 = a < 10;
+           bool c2 = a < 200;
+           bool c3 = a < 30;
+
+           Console.WriteLine(c1);
+           Console.WriteLine(c2);
+
+        }
+    }
+}
