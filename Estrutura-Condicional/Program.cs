e30463a0b3a9ab577f03e30a2f4e7c5105400618
Elton Andrade <eltonsa75@hotmail.com>
2019-08-07 13:58:45 -0300

    Estrutura-Simples

---
+++ Estrutura-Condicional/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Estrutura_Condicional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int x = 10;
+
+            Console.WriteLine("Good Mornig");
+            if (x > 5) {
+            Console.WriteLine("Good Everning");
+        }
+
+         Console.WriteLine("Good Night");
+        }
+    }
+}
