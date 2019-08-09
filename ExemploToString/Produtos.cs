b02e670d4dd26555f23088fd027b51795695996a
Elton Andrade <eltonsa75@hotmail.com>
2019-08-09 19:36:52 -0300

    Exemplo Adiconar - Remover Produtos

--- ExemploToString/Produtos.cs
+++ ExemploToString/Produtos.cs
@@ -13,6 +13,14 @@ namespace ExemploToString{
             return Preco * Quantidade;
         }
 
+        public void AdicionarProdutos(int quantidade){
+            Quantidade += quantidade;
+        }
+
+          public void RemoverProdutos(int quantidade){
+            Quantidade -= quantidade;
+        }
+
         public override string ToString(){
             return Nome 
             + ", $ "
