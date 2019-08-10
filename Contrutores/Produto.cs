dfa7d4ad0f38443bac699d8ad0685e35d0122210
Elton Andrade <eltonsa75@hotmail.com>
2019-08-10 11:28:49 -0300

    Construtor

---
+++ Contrutores/Produto.cs
@@ -0,0 +1,41 @@
+using System.Globalization;
+
+namespace Contrutores
+{
+    class Produto{
+
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto(string nome, double preco, int quantidade){
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        // Method Preço * Valor
+
+        public double ValorTotalEmEstoque(){
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade){
+            Quantidade += quantidade;
+        }
+
+          public void RemoverProdutos(int quantidade){
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString(){
+            return Nome 
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
